6e92c7c7601ae94781fccc3f00218554140e8c64
Charles Hood <charles.hood@gmail.com>
2020-01-15 21:59:06 -0500

    Revert "add gatsby-plugin-manifest"
    This reverts commit ac52c1badfe87f4eca89e89fc0cae6fc3d4eaec1.

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,27 +24,6 @@ module.exports = {
         // pwaName: null
       }
     },
-
-    // Add favicon and other PWA manifest config
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "Thomas Hinds Media",
-        short_name: "thmshinds",
-        start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/images/favicon.png", // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
-      },
-    },
-
      // Add gatsby-plugin-mailchimp
     {
       resolve: 'gatsby-plugin-mailchimp',
