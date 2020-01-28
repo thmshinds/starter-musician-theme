b42709e8dd2f2921d34d9e28f3cfd156ad7f9cf9
Charles Hood <charles.hood@gmail.com>
2020-01-27 20:09:27 -0500

    Removed conflicting shadow components
    :-(

--- src/gatsby-theme-musician/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Banner } from "./banner"
-export { default as Container } from "./container"
-export { default as Grids } from "./grids"
-export { default as Releases } from "./releases"
-export { default as Shows } from "./shows"
-export { default as Social } from "./social"
-export { default as Youtube } from "./youtube"
